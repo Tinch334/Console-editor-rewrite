2f2d689dc97854ea443479a1aedb2226c43b3658
Martin Goñi <screadores122@gmail.com>
2022-11-19 18:13:49 -0300

    Added line numbers

--- display.py
+++ display.py
@@ -1,4 +1,4 @@
-import utils, curses, curses.ascii
+import utils, math
 from dataclasses import dataclass
 from typing import Any
 
@@ -15,12 +15,16 @@ class BufferDisplayConfig:
     x_start: int = 0
     x_end: int = 0
 
+    line_number_min_width = 2
+
 
 #This class contains colour configuration for the various elements in the editor.
 @dataclass
 class BufferColourConfig:
     text_colour = "WHITE_BLACK"
     cursor_colour = "BLACK_WHITE"
+    line_number_colour = "BLACK_WHITE"
+    empty_line_colour = "WHITE_BLACK"
 
 
 class Display:
@@ -42,6 +46,11 @@ class Display:
 
     #This function calls all the different display functions, it's the one that should be called from the editor.
     def display(self) -> None:
+        #The line numbers are printed first because in the process of printing them their width is calculated, that value is then used to print
+        #the buffer and cursor. We can call this function before calculating the scroll because we aren't printing the buffer, cursor or
+        #anything related to scroll.
+        self.display_line_nums()
+
         #Before printing anything we update the scroll variables.
         self.scroll_handler()
 
@@ -80,6 +89,29 @@ class Display:
                 break
 
 
+    #Displays line numbers an calculates "buffer_config.x_start".
+    def display_line_nums(self) -> None:
+        #The number of characters required to display the maximum number of lines.
+        line_number_width = max(int(math.log10(self.buffer.get_line_count())) + 1, self.buffer_config.line_number_min_width)
+        #We set the x_start according to the width of the line number.
+        self.buffer_config.x_start = line_number_width
+
+        end_y = self.editor.y_size + self.buffer_config.y_end
+
+        for y in range(self.buffer_config.y_start, end_y):
+            #In case we are at the very end of the buffer, and there's empty space before the status-bar.
+            if y + self.buffer_y_scroll < self.buffer.get_line_count():
+                num_width = int(math.log10(y + 1)) + 1
+                padding = " " * (line_number_width - num_width - self.buffer_y_scroll)
+                #To calculate the current line number we add the buffer scroll to the y counter, that way it remains correct even when the
+                #buffer is scrolled vertically. Finally we add 1 to account for the fact that line numbers start at 1, not 0.
+                line_number = y + self.buffer_y_scroll + 1
+
+                self.editor.stdscr.addstr(y, 0, f"{padding}{line_number}", self.editor.get_colour(self.colour_config.line_number_colour))
+            else:
+                self.editor.stdscr.addstr(y, 0, "~", self.editor.get_colour(self.colour_config.empty_line_colour))
+
+
     #Displays the cursor
     def display_cursor(self) -> None:
         cursor_char = " "
@@ -90,8 +122,8 @@ class Display:
         if cursor_x != len(self.buffer.get_line(cursor_y)) and len(self.buffer.get_line(cursor_y)) != 0:
             cursor_char = self.buffer.get_char(cursor_y, cursor_x)
 
-        #When calculating the Y position of the cursor we must consider the current Y scroll of the buffer.
-        self.editor.stdscr.addstr(cursor_y - self.buffer_y_scroll, cursor_x - self.buffer_x_scroll, cursor_char, self.editor.get_colour(self.colour_config.cursor_colour))
+        #When calculating the position of the cursor we must consider the current scroll of the buffer.
+        self.editor.stdscr.addstr(cursor_y - self.buffer_y_scroll, cursor_x + self.buffer_config.x_start - self.buffer_x_scroll, cursor_char, self.editor.get_colour(self.colour_config.cursor_colour))
 
 
     #Handles the horizontal and vertical scroll for printing the appropriate part of the buffer depending on the position of the cursor. 
@@ -111,8 +143,10 @@ class Display:
         elif cursor_y < self.buffer_y_scroll:
             self.buffer_y_scroll = cursor_y
 
-        if cursor_x > end_x + self.buffer_x_scroll - 1:
-            self.buffer_x_scroll = cursor_x - end_x + 1
+        #Same concept except in the X axis, except that we also take into account the fact that "start_x" can be not zero, when the line
+        #numbers are enabled.
+        if cursor_x > end_x - self.buffer_config.x_start + self.buffer_x_scroll - 1:
+            self.buffer_x_scroll = cursor_x - end_x + self.buffer_config.x_start + 1 
         #If the cursor goes above the printed part of the buffer we set the scroll to the cursor's position, which is just enough for the cursor
         #to appear on the first line.
         elif cursor_x < self.buffer_x_scroll:
